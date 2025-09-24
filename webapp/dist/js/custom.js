5ab3819e4662d8fcd7a5756c22a92f5cbb24699d
studioantoinedamay <bonjour@antoinedamay.fr>
2025-09-24 22:25:43 +0200

    Delete webapp/dist directory

--- webapp/dist/js/custom.js
+++
@@ -1,8 +0,0 @@
-CONFIG = {
-    // Title of the tab and logo
-    "TITLE": "Plik",
-
-    // Disable the authentication button
-    // Authentication page can still be accessed at https://plik.tld/#/login if enabled
-    "DISABLE_AUTH_BUTTON": false
-}
